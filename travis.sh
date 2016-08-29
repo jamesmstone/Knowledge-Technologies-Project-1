72a0a44453373d60ae5c2dd2862adfe988516475
James Stone <jamesmstone@users.noreply.github.com>
2016-08-29 23:33:05 +1000

    make travis run long commands at the same time!

---
+++ travis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+build_pdf(){
+  docker pull blang/latex;
+  docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$(pwd)/report":/data blang/latex xelatex -interaction=nonstopmode report.tex || true; test -e report/report.pdf && echo "pdf generated"; #  change fail condition to not having a pdf generated
+  cat report/report.log;
+}
+
+build_tweets_ouput(){
+  ./neighbourhoodSearch.sh ./tweets/stone1_tweets_small.txt ./geonames/US-loc-names.txt 1 2>/dev/null  > output.txt;
+  cat output.txt;
+}
+
+build_pdf &
+build_tweets_ouput &
+wait $(jobs -p);
